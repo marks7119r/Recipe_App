7caab354703ebb64ec6c2b97dd259cce5e4b1a1a
MARK SAFWAT TANIOUS FILIBS <mark.filibs@student.aast.edu>
2023-12-30 14:14:43 +0200

    another one

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.recipe_app {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires com.dlsc.formsfx;
+
+    opens com.example.recipe_app to javafx.fxml;
+    exports com.example.recipe_app;
+}
