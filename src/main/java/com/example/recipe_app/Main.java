002a96843604af7e0e9795781eb71e2f5cda94f7
MARK SAFWAT TANIOUS FILIBS <mark.filibs@student.aast.edu>
2023-12-30 17:00:18 +0200

    updated RecipeManager

--- src/main/java/com/example/recipe_app/Main.java
+++ src/main/java/com/example/recipe_app/Main.java
@@ -1,7 +1,20 @@
 package com.example.recipe_app;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+public class Main extends Application {
     public static void main(String[] args) {
-        RecipeManager.main(args);
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Create your JavaFX user interface here
+        // You can use FXML for the UI or create it programmatically
+
+        primaryStage.setTitle("Recipe Manager");
+        // Add your UI components here
+
+        primaryStage.show();
     }
 }
--- src/main/java/com/example/recipe_app/RecipeManager.java
+++ src/main/java/com/example/recipe_app/RecipeManager.java
@@ -2,20 +2,38 @@ package com.example.recipe_app;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import java.util.List;
 
 
-public class RecipeManager extends Application {
-    public static void main(String[] args) {
-        launch(args);
+public class RecipeManager  {
+    private List<Recipe> recipes;
+    private List<RecipeCategory> categories;
+
+    // Methods for managing recipes and categories
+    public void addRecipe(Recipe recipe) {
+        // Add the recipe to the list and database
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-        // UI initialization
-        primaryStage.setTitle("Recipe Manager");
-        // Add your UI components here
-
-        primaryStage.show();
+    public void editRecipe(Recipe recipe) {
+        // Edit the recipe in the list and update the database
     }
+
+    public void deleteRecipe(Recipe recipe) {
+        // Remove the recipe from the list and database
+    }
+
+    public void addCategory(RecipeCategory category) {
+        // Add the category to the list and database
+    }
+
+    public void editCategory(RecipeCategory category) {
+        // Edit the category in the list and update the database
+    }
+
+    public void deleteCategory(RecipeCategory category) {
+        // Remove the category from the list and database
+    }
+
+    // Other methods for managing recipes and categories
 
 }
